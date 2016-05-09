2e8fc6336c4900e236471d4329c5023f37c78f3d
Jared Pochtar <jpochtar@gmail.com>
2016-05-09 17:05:54 -0400

    benchmark refactoring

--- client/benchmark.py
+++ client/benchmark.py
@@ -1,15 +1,29 @@
 import concurrent.futures
+
+# FIXME this is going to measure CPU time; we need a timer that will measure
+# Wall time
 from timeit import default_timer as timer
+
 import kvs_client
 import psycopg2
 import redis
 
-def benchmark(client, fn, num_transactions=1000, num_workers=5):
+# FIXME using timeit.default_timer will result in the wrong time (!!)
+def timed(fn):
+    def wrapped(*args, **kwargs):
+        start = timer()
+        fn(*args, **kwargs)
+        latency = timer() - start
+        return latency
+    return wrapped
+
+
+def benchmark(client, fn, num_transactions=20, num_workers=100):
     start = timer()
     total_latency = 0
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
-        futures = [executor.submit(fn, client) for _ in range(num_transactions)]
+        futures = [executor.submit(timed(fn), client) for _ in range(num_transactions)]
 
         for future in concurrent.futures.as_completed(futures):
             total_latency += future.result()
@@ -21,8 +35,6 @@ def benchmark(client, fn, num_transactions=1000, num_workers=5):
         print("Throughput: {} transactions per second".format(throughput))
 
 def spawn_kvs_transaction(client):
-    start = timer()
-
     try:
         txn = kvs_client.Transaction(client)
         a = int(txn.get('/a') or 0)
@@ -32,8 +44,6 @@ def spawn_kvs_transaction(client):
     except kvs_client.TransactionFailureException:
         pass
 
-    latency = timer() - start
-    return latency
 
 def initialize_postgres(conn):
     with conn.cursor() as cur:
@@ -42,8 +52,6 @@ def initialize_postgres(conn):
         cur.execute("INSERT INTO keys VALUES('b',0);")
 
 def spawn_postgres_transaction(conn):
-    start = timer()
-
     with conn.cursor() as cur:
         cur.execute("SELECT * from keys WHERE key='a';")
         _, a = cur.fetchone()
@@ -51,12 +59,7 @@ def spawn_postgres_transaction(conn):
         _, b = cur.fetchone()
         cur.execute("UPDATE keys SET value={} WHERE key='b';".format(a + b + 10))
 
-    latency = timer() - start
-    return latency
-
 def spawn_redis_transaction(client):
-    start = timer()
-
     with client.pipeline() as pipe:
         try:
             pipe.watch('/a')
@@ -69,9 +72,6 @@ def spawn_redis_transaction(client):
         except redis.WatchError:
             pass
 
-    latency = timer() - start
-    return latency
-
 if __name__ == '__main__':
     print("-------------------")
     print("Benchmarking KVS...")
@@ -82,7 +82,7 @@ if __name__ == '__main__':
     print("------------------------")
     print("Benchmarking Postgres...")
     print("------------------------")
-    conn = psycopg2.connect(dbname='postgres')
+    conn = psycopg2.connect(dbname='kvtransactional')
     initialize_postgres(conn)
     benchmark(conn, spawn_postgres_transaction)
 
